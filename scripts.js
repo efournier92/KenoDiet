d41c706c6af9bf874dd7560dfcd26fa742f41824
Eric Fournier <efournier92@gmail.com>
2018-05-22 15:35:12 -0400

    refactor scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 function updateCountdown() {
   var d = new Date();
-  var seconds = d.getMinutes() * 60 + d.getSeconds(); //convet 00:00 to seconds for easier caculation
-  var fiveMin = 60 * 20; //five minutes is 300 seconds!
-  var timeleft = fiveMin - seconds % fiveMin; // let's say 01:30, then current seconds is 90, 90%300 = 90, then 300-90 = 210. That's the time left!
-  var result = parseInt(timeleft / 60) + ':' + (timeleft % 60).pad(2); //formart seconds into 00:00 
+  var seconds = d.getMinutes() * 60 + d.getSeconds(); // convert time to seconds
+  var fiveMin = 60 * 20; // reset every 20 minutes
+  var timeleft = fiveMin - seconds % fiveMin;
+  var result = parseInt(timeleft / 60) + ':' + (timeleft % 60).pad(2); //format seconds
   document.getElementById('countdown').innerHTML = result;
 }
 
+// refresh countdown once per second
 setInterval(function () {
   updateCountdown()
 }, 1)
